27c6cb34b9d6aba767f9e2b9e556908f5ad4f80c
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-10-09 11:11:41 +0200

    test 01-template

--- 02-bases/tests/app.test.ts
+++ 02-bases/tests/app.test.ts
@@ -1,7 +1,17 @@
-describe('App', () => {
-  it('should be true', () => {
+// AAA
 
-    expect(true).toBe(true);
+describe('Test in the App File', () => {
+
+  it('should be 30', () => {
+    // 1. Arrange
+    const num1 = 10;
+    const num2 = 20;
+
+    // 2. Act
+    const result = num1 + num2;
+
+    // 3. Assert
+    expect(result).toBe(30);
     
   });
 });
