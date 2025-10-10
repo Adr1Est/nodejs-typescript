b10c801260877ba67e97d25722b830ddaa06371b
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-10-10 10:04:37 +0200

    testing 06-promises

---
+++ 02-bases/tests/06-promises.test.ts
@@ -0,0 +1,24 @@
+import { getPokemonById } from '../src/js-foundation/06-promises'
+
+describe('js-foundation/06-promises', () => {
+
+  test('getPokemonById should return a pokemon', async () => {
+    const pokemonId = 1;
+    const pokemonName = await getPokemonById(pokemonId);
+
+    expect(pokemonName).toBe('bulbasaur');
+  });
+
+  test('should return an error if pokemon does not exist', async () => {
+    const pokemonId = 100000;
+
+    try{
+      const pokemonName = await getPokemonById(pokemonId);
+      expect(true).toBeFalsy;
+    }catch(error){
+      expect(error).toBe('Pokemon no existe')
+    }
+
+  })
+
+})
