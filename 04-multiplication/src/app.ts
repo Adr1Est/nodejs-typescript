626d1020831960f61fd86eeeddad2c7edcdbcff4
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-10-10 13:03:15 +0200

    app consola 04-multiplication

--- 04-multiplication/src/app.ts
+++ 04-multiplication/src/app.ts
@@ -1,3 +1,22 @@
-const message = 'Hola mundo';
+import fs from 'fs';
 
-console.log(message);
+const generarTablaMultiplicar = (num: number) => {
+  let content = `
+======================================\n
+TABLA DEL ${num}\n
+======================================\n
+\n`;
+
+  for (let i = 1; i < 10 ; i++){
+    const result = num * i;
+    content += `${num} + ${i} = ${result}\n`
+  }
+
+  fs.writeFileSync(`outputs/tabla-${num}.txt`, content);
+  console.log('Archivo creado!');
+  
+
+  return content;
+}
+
+console.log(generarTablaMultiplicar(5));
