13f9941de7a7050bdc0f925860f5a39fb84b77cd
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-10-21 12:54:03 +0200

    mongo datasource

--- 05-NOC/src/app.ts
+++ 05-NOC/src/app.ts
@@ -1,3 +1,5 @@
+import { envs } from './config/plugins/envs.plugin';
+import { LogModel, MongoDatabase } from './data/mongo';
 import { Server } from './presentation/server';
 
 
@@ -5,8 +7,25 @@ import { Server } from './presentation/server';
   main();
 })();
 
+async function main(){
 
-function main(){
+  await MongoDatabase.connect({
+    mongoURL: envs.MONGO_URL,
+    dbName: envs.MONGO_DB_NAME,
+  });
+
+  // Crear una colección (tabla), documento = registro
+
+  // const newLog = await LogModel.create({
+  //   message: 'Test message from mongoose',
+  //   origin: 'App.ts',
+  //   level: 'low',
+  // });
+
+  // await newLog.save();
+  const logs = await LogModel.find();
+  console.log(logs);
+
   Server.start();
 }
 
