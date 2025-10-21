560cc0934a5af444e08d51e03caf4f4050544f6f
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-10-21 13:52:44 +0200

    NOC - mongo - postgres

--- 05-NOC/src/app.ts
+++ 05-NOC/src/app.ts
@@ -1,3 +1,4 @@
+import { PrismaClient } from './generated/prisma';
 import { envs } from './config/plugins/envs.plugin';
 import { LogModel, MongoDatabase } from './data/mongo';
 import { Server } from './presentation/server';
@@ -9,24 +10,41 @@ import { Server } from './presentation/server';
 
 async function main(){
 
-  await MongoDatabase.connect({
-    mongoURL: envs.MONGO_URL,
-    dbName: envs.MONGO_DB_NAME,
-  });
-
-  // Crear una colección (tabla), documento = registro
-
-  // const newLog = await LogModel.create({
-  //   message: 'Test message from mongoose',
-  //   origin: 'App.ts',
-  //   level: 'low',
+  // await MongoDatabase.connect({
+  //   mongoURL: envs.MONGO_URL,
+  //   dbName: envs.MONGO_DB_NAME,
   // });
 
-  // await newLog.save();
-  const logs = await LogModel.find();
+  // // Crear una colección (tabla), documento = registro
+
+  // // const newLog = await LogModel.create({
+  // //   message: 'Test message from mongoose',
+  // //   origin: 'App.ts',
+  // //   level: 'low',
+  // // });
+
+  // // await newLog.save();
+  // const logs = await LogModel.find();
+  // console.log(logs);
+
+  const prisma = new PrismaClient();
+  // const newLog = await prisma.logModel.create({
+  //   data: {
+  //     level: 'HIGH',
+  //     message: 'Test message',
+  //     origin: 'App.ts'
+  //   }
+  // });
+  // console.log({ newLog });
+
+  const logs = await prisma.logModel.findMany({
+    where:{
+      level: 'MEDIUM'
+    }
+  });
   console.log(logs);
 
-  Server.start();
+  // Server.start();
 }
 
 
